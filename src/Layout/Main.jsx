f5f9da2d9b36f872d45f2cfec2317abe7418debe
MUEID11 <mehedirangpur3@gmail.com>
2024-09-18 19:00:36 +0600

    delete developement

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 const Main = () => {
   return (
     <div>
+      <h1>Hellow team CodeWarriors</h1>
       {/* Navbar */}
       <Outlet />
       {/*Footer*/}
