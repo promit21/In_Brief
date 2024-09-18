5f3f9681aff3473fdf9537e1fde5192da7bb0fd4
MUEID11 <mehedirangpur3@gmail.com>
2024-09-18 18:23:24 +0600

    fixed tailwind

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 const Main = () => {
   return (
     <div>
-      <div className="Text-5xl font-bolt text-center">hello team CodeWarriors</div>
+      <div className="text-5xl font-bold text-center">hello team CodeWarriors</div>
       {/* Navbar */}
       <Outlet />
       {/*Footer*/}
