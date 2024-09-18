023c678aa4b8e21f177d49b7beac71c5ab7c4435
MUEID11 <mehedirangpur3@gmail.com>
2024-09-18 19:01:38 +0600

    dev

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 const Main = () => {
   return (
     <div>
-      <h1>Hellow team CodeWarriors</h1>
       {/* Navbar */}
       <Outlet />
       {/*Footer*/}
