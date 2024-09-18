28b6085499417b8d9b4f580fee19e98d11e14769
MUEID11 <mehedirangpur3@gmail.com>
2024-09-18 18:00:14 +0600

    trying to fix tailwind problem

--- src/Router/route.jsx
+++ src/Router/route.jsx
@@ -2,16 +2,12 @@ import {
   createBrowserRouter,
 } from "react-router-dom";
 import Main from "../Layout/Main";
-import Register from "../Pages/Register";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
     children: [
-      {
-        path:'register',
-        element: <Register />,
-      }
+
     ]
   },
 ]);
