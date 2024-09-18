f0cf9af029e8cd29de9aef3547bb3d3b27b104c2
MUEID11 <mehedirangpur3@gmail.com>
2024-09-18 18:55:52 +0600

    log in and registration page

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 const Main = () => {
   return (
     <div>
+      <h1>hellow world</h1>
       {/* Navbar */}
       <Outlet />
       {/*Footer*/}
--- src/Router/route.jsx
+++ src/Router/route.jsx
@@ -1,16 +1,22 @@
-import {
-  createBrowserRouter,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
+import Register from "../Pages/Register";
+import Signin from "../Pages/Signin";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
-    Children: [
+    errorElement: <h1>Oh no 404 error</h1>,
+    children: [
       {
-
-      }
-    ]
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/signin",
+        element: <Signin />,
+      },
+    ],
   },
 ]);
 
